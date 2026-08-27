f63d1db2ee9fb49d3654f8263e02dd130c2fc6bc
navadeez <navadeeshan2320@gmail.com>
2026-08-27 13:55:20 +0530

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-WN1is2qgA6dQN4-oMLUPCB6eLFwuo3E",
+  apiKey: "AIzaSyB-WN1is2qA6dQN4-oMLUPCB6eLFwuo3E",
   authDomain: "stars-fabric-library-e2d2c.firebaseapp.com",
   projectId: "stars-fabric-library-e2d2c",
   storageBucket: "stars-fabric-library-e2d2c.firebasestorage.app",
@@ -15,3 +13,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore
+export const db = getFirestore(app);
